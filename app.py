10ebc04001c4e63e7df9abe5e91a2049cb76ff13
Mimbouré Yara <86371554+MimboureYara@users.noreply.github.com>
2024-08-07 18:19:26 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("Mon premier pas en streamlit")
+def main():
+    st.write("Mon premier pas en streamlit")
+    ser1 = pd.Series(list('abcedfghijklmnopqrstuvwxyz'))
+    ser2 = pd.Series(np.arange(26))
+    df = pd.DataFrame({"lettre":ser1, "num":ser2}).reset_index()
+    st.dataframe(df)
+    st.write("Lettres d'alphabet en fonction du numero d'ordre")
+    st.line_chart(df.lettre)
+
+if __name__ == '__main__':
+    main()
